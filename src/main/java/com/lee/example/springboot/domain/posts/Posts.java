928c43813e7420cd0565bfc104ebc1de82a58c09
1eedaegon <d8726243@gmail.com>
2021-04-06 21:53:28 +0900

    feat: Add save post on SpringJPA
    게시판 프로젝트를 위한 데이터 저장소 설정
    hibernate mysql innoDB설정 및
    jdbc설정 추가

--- src/main/java/com/lee/example/springboot/domain/posts/Posts.java
+++ src/main/java/com/lee/example/springboot/domain/posts/Posts.java
@@ -1,5 +1,33 @@
 package com.lee.example.springboot.domain.posts;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class Posts {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+    // @Entity 클래스는 Column annotation을 작성하지 않아도 기본적으로 Column으로 생성된다.
+    // 기본 설정은 VARCHAR(255)이며 설정을 바꾸고 싶으면 @Column을 사용하면 된다.
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
 }
---
+++ src/main/java/com/lee/example/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.lee.example.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {}
