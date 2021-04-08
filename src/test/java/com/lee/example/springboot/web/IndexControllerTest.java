ee6b2f35025fd4650489330df8a73e80cf0bd433
1eedaegon <d8726243@gmail.com>
2021-04-08 21:27:41 +0900

    feat: Add index teplate using mustache
    "/" 페이지 응답을 위한 mustache 템플릿 사용

---
+++ src/test/java/com/lee/example/springboot/web/IndexControllerTest.java
@@ -0,0 +1,28 @@
+package com.lee.example.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지를_보여줘야_한다() {
+
+        // When
+        String body = this.restTemplate.getForObject("/", String.class);
+        // Then
+        assertThat(body).contains("Getting");
+    }
+}
